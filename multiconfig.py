68638658283090168c0904c609617f30576a9839
Alexey Stankevich <alexey.stn@gmail.com>
2021-08-04 23:15:33 +0300

    Upload diffs to FC

--- multiconfig.py
+++ multiconfig.py
@@ -2,8 +2,11 @@ import serial
 import time
 import json
 import sys
+import os
 
 import serial.tools.list_ports
+
+ports_already_used = []
 
 def wait_for_new_port():
     ports_current = set()
@@ -53,35 +56,99 @@ def get_mcu_id(port_name):
                     name = s.strip()[7:]
                     break
     return mcu_id, name
-                
+
+
+def open_advanced(filename):
+    try:
+        f = open(filename, 'r')
+        return(f)
+    except:
+        print('Prepare "' + filename + '" before using this app')
+        f = open(filename, 'w')
+        f.close()
+        input()
+        sys.exit(0)
+
 
 def load_settings():
     settings = {}
     key = None
-    with open('settings.txt') as f:
-        for line in f:
-            if not (line.startswith('\t') or line.startswith(' ')):
-                key = line.strip()
-                settings[key] = []
-            elif len(line.strip()) > 0:
-                settings[key].append(line.strip())
+
+    f = open_advanced('settings.txt')
+    for line in f:
+        if not (line.startswith('\t') or line.startswith(' ')):
+            key = line.strip()
+            settings[key] = []
+        elif len(line.strip()) > 0:
+            settings[key].append(line.strip())
+    f.close()
     return settings
 
 
 def load_diff():
     diff = []
-    with open('diff.txt') as f:
-        for line in f:
-            line = line.strip()
-            if len(line) > 0 and not line.startswith('#'):
-                diff.append(line)
+    f = open_advanced('diff.txt')
+    for line in f:
+        line = line.strip()
+        if len(line) > 0 and not line.startswith('#'):
+            diff.append(line)
+    f.close()
+    if len(diff) == 0:
+        print('Diff file is empty')
+        input()
+        sys.exit(0)
     if diff[-1] != 'save':
         print('Diff file does not end with "save" command')
+        input()
+        sys.exit(0)
     return diff
 
 
-#settings = load_settings()
-#diff = load_diff()
+def get_line_timeout(ser):
+    t = time.time() + 1.0
+    while time.time() < t:
+        try:
+            get_line()
+        except:
+            break
+
+
+def get_line(ser):
+    s = b''
+    while ser.in_waiting:
+        a = ser.read()
+        s += a
+    s = s.decode()
+    s = s.replace('\r\n\r\n', '\r\n')
+    if not os.name == 'nt':
+        s = s.replace('\r','')
+    print(s, end='')
+    time.sleep(0.01)
+
+
+def upload_diff(diff, port):
+    with serial.Serial(port, 115200, timeout=1) as ser:
+        ser.write(b'#cli\r\n')
+        time.sleep(1)
+        ser.reset_input_buffer()
+        for line_out in diff:
+            ser.write(line_out.encode() + b'\r\n')
+            get_line(ser)
+        get_line_timeout(ser)
+
+
+def clear():
+    if os.name == 'nt': # for windows
+        os.system('cls')
+    else: # for mac and linux(here, os.name is 'posix')
+        os.system('clear')
+
+
+clear()
+settings = load_settings()
+diff = load_diff()
+custom_line = '############# CUSTOM SETTINGS #############'
+
 
 while True:
     port = wait_for_new_port()
@@ -89,14 +156,23 @@ while True:
     print('Press Enter to read Name and ID', end='')
     input()
     mcu_id, name = get_mcu_id(port)
-    print('MCU ID: {0}'.format(mcu_id))
-    print('Name: {0}'.format(name))
+    print('MCU ID: {0}'.format(mcu_id), end = '')
+    #print('Name: {0}'.format(name))
+    diff_current = diff.copy()
+    if mcu_id in settings.keys():
+        print(' - found')
+        for s in settings[mcu_id]:
+            print('   ', s)
+        print('Press Enter to load CUSTOM diff', end='')
+        input()
+        diff_current.insert(-1, custom_line)
+        for s in settings[mcu_id]:
+            diff_current.insert(-1, s)
+    else:
+        print(' - not found')
+        print('Press Enter to load DEFAULT diff', end='')
+        input()
+    upload_diff(diff_current, port)
     print()
-    #print(mcu_id in config.keys())
-
-
-#with open('config.json') as f:
-#    students = json.load(f)
-
-
+    print()
 
